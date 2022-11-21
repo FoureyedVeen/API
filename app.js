0ef2f00c56ded1c565b82b17f217a83f2a746017
Godswill <godswilll@analogrepublic.com>
2022-11-21 08:57:32 +0100

    changed API and logged data

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const api_url = "https://employeedetails.free.beeceptor.com/my/api/path";
+const api_url = "https://jsonplaceholder.typicode.com/users";
 
 async function getapi(url) {
     
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+  
 }
+
+// Calling that async function
+getapi(api_url);
